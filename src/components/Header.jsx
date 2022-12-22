71f85b5b22919c01490130cdc2d5fc2ebbd19891
rohityellapu <rohithyellapu24@gmail.com>
2022-12-22 19:12:34 +0530

    configured logout button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { AiOutlineUser } from 'react-icons/ai'
 import { FiChevronDown } from 'react-icons/fi'
 import { useNavigate } from 'react-router-dom'
 function Header({ username, userId }) {
   const navigate = useNavigate()
+  const [isLogout, setLogout] = useState(false)
   function logout() {
     localStorage.clear()
     navigate('/login')
@@ -14,9 +15,14 @@ function Header({ username, userId }) {
         <div className="user-id lg-font">
           <h6>USER ID : { userId}</h6>
         </div>
-        <div className="user-name lg-font">
-          <h6 className='flex flex-row items-center'><div><AiOutlineUser color='#A6A6A6' size={ 22 } /></div><div>User Name : { username }</div><div><FiChevronDown color='#A6A6A6' /></div> </h6>
-        <button onClick={logout}>Logout</button>
+        <div className="user-name lg-font flex flex-column">
+          <h6 className='flex flex-row items-center'>
+            <div><AiOutlineUser color='#A6A6A6' size={ 22 } /></div>
+            <div className='username' onClick={() => setLogout(prev => !prev)}>{ username }</div>
+            <div><FiChevronDown color='#A6A6A6' /></div>
+          </h6>
+          { isLogout && <button className='logout-btn' onClick={ logout }>Logout</button> }
+        
         </div>
         
       </div>
