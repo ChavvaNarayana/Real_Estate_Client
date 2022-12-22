57540796e1b353c134dcca7f5bf3b403f02df960
Harish238 <118815836+Harish238@users.noreply.github.com>
2022-12-22 17:36:34 +0530

    nothing changed

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,53 +1,54 @@
 import React, { useState } from 'react';
-import './src/components/Signup.css';
+import './Signup.css';
 import axios from "axios";
-const Signup = async()=> {
-  const[data,setData] = useState({
-    username:'',
-    email:'',
-    password:'',
-    confirmPassword:'',
-  })
-  const res = await axios.post(
-    ``,
-    data,
-    {
-      withCredentials: true,
-    });
-  const{username,email}=data;
-  const{password,confirmPassword} = Number;
-  const changeHandler = e =>{
-    setData({...data,[e.target.name]:[e.target.value]})
-    setData({...Number,[e.target.password]:[e.target.value]})
-  }
-  const submitHandler = e =>{
-    e.preventDefault();
-   if(password===confirmPassword){
-    console.log(data);
-   }
-   else {
-    console.log("Passwords not matching")
-   }
-  }
+import { useNavigate } from 'react-router-dom';
+const apiUrl = 'http://localhost:5000/register'
+const Signup = () => {
+    const navigate = useNavigate()
+    const [data, setData] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    })
+    const changeHandler = e => {
+        const { name, value } = e.target
+        setData(data => {
+            return { ...data, [name]: value }
+        })
 
-  return (
-    <div className="sign-up-parent">
-      <div className="sign-up-form-container">
-      <center>
-        <h1 style={{ color: "#4c57b6" }}>Logo</h1>
-        <p>Create New Account</p>
-       <form onSubmit={submitHandler}>
-        <input type="text" name='username' placeholder='username' value={username} onChange={changeHandler}/> <br/>
-        <input type="email" name='email' placeholder='email id' value={email} onChange={changeHandler}/><br/>
-        <input type="password" name='password' placeholder='password' value={password} onChange={changeHandler}/><br/>
-        <input type="Password" name='confirmPassword' placeholder='confirmpassword' value={confirmPassword} onChange={changeHandler}/><br/>
-        <button type="submit" className="submit-button">Sign Up</button>
-       </form>
-      </center>
-      </div>
-    </div>
-  )
-    
+    }
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        await axios.post(apiUrl, { data }).then(res => res.data)
+            .then(data => {
+                localStorage.setItem('userId', data.ppdId)
+                localStorage.setItem('email', data.email)
+                localStorage.setItem('username', data.username)
+                localStorage.setItem('token', data.token)
+                navigate('/')
+            })
+            .catch(err => console.log(err))
+    }
+
+    return (
+        <div className="sign-up-parent" >
+            <div className="sign-up-form-container">
+                <center>
+                    <h1 style={ { color: "#4c57b6" } }>Logo</h1>
+                    <p>Create New Account</p>
+                    <form onSubmit={ submitHandler }>
+                        <input type="text" name='username' placeholder='username' value={ data.username } onChange={ changeHandler } /> <br />
+                        <input type="email" name='email' placeholder='email id' value={ data.email } onChange={ changeHandler } /><br />
+                        <input type="password" name='password' placeholder='password' value={ data.password } onChange={ changeHandler } /><br />
+                        <input type="Password" name='confirmPassword' placeholder='confirmpassword' value={ data.confirmPassword } onChange={ changeHandler } /><br />
+                        <button type="submit" className="submit-button">Sign Up</button>
+                    </form>
+                </center>
+            </div>
+        </div >
+    )
 }
+
 
 export default Signup;
