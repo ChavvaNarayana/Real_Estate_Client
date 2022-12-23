3238f19b59bffc57b9c650ff670eb01f06ddaccb
rohityellapu <rohithyellapu24@gmail.com>
2022-12-23 13:00:33 +0530

    added new logo in menubar

--- src/components/Dashboard/menubar.js
+++ src/components/Dashboard/menubar.js
@@ -6,57 +6,57 @@ import { BsFillTagFill } from "react-icons/bs";
 
 import { GrFormView, GrUpload, GrDownload } from "react-icons/gr";
 import "./menubar.css";
-import logo from "./real-estate.jpg";
+import logo from "../../images/Logo.png";
 
 const Menubar = () => {
   return (
     <>
       <div className="sidebarconatiner">
-        <div className="logooption">
+        <div className="logo-option">
           <img src={logo} alt="Logo" style={{ width: "70px" }} />
         </div>
 
         <div className="sidesubcontainer">
           <div className="sideoption property">
             <div className="home">
-              <RiHome3Line />
+              <RiHome3Line size={20} />
             </div>
-            <div className="property">Property</div>
+            <div className="property font">Property</div>
           </div>
 
-          <div className="sideoption gray">
-            <div>
-              <IoIosNotifications />
+          <div className="sideoption">
+            <div className="other-icons">
+              <IoIosNotifications  size={ 20 } />
             </div>
-            <div className="navvalue">Assistant</div>
+            <div className="navvalue font">Assistant</div>
           </div>
 
-          <div className="sideoption gray">
-            <div className="colorgray">
-              <GrDownload />
+          <div className="sideoption">
+            <div className="other-icons">
+              <GrDownload  size={ 20 } />
             </div>
-            <div className="navvalue">Received Interest</div>
+            <div className="navvalue font">Received Interest</div>
           </div>
 
-          <div className="sideoption gray ">
-            <div className="colorgray">
-              <GrUpload />
+          <div className="sideoption ">
+            <div className="other-icons">
+              <GrUpload size={ 20 } />
             </div>
-            <div className="navvalue">Sent Interest</div>
+            <div className="navvalue font">Sent Interest</div>
           </div>
 
-          <div className="sideoption gray">
-            <div>
-              <GrFormView />
+          <div className="sideoption">
+            <div className="other-icons">
+              <GrFormView size={ 20 } />
             </div>
-            <div className="navvalue">Property View</div>
+            <div className="navvalue font">Property View</div>
           </div>
 
-          <div className="sideoption gray">
-            <div>
-              <BsFillTagFill />
+          <div className="sideoption">
+            <div className="other-icons">
+              <BsFillTagFill size={ 20 } />
             </div>
-            <div className="navvalue">Traffic Plan</div>
+            <div className="navvalue font">Traffic Plan</div>
           </div>
         </div>
       </div>
